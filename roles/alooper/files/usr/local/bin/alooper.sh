ff882c6028f4258d4cde29a596cc04c0e04c4fb9
Daenou <github@drth.ch>
2021-10-29 13:32:07 +0200

    Use  in bluealsa as well

--- roles/alooper/files/usr/local/bin/alooper.sh
+++ roles/alooper/files/usr/local/bin/alooper.sh
@@ -18,7 +18,7 @@ fi
 
 while true; do
 	case "$1" in
-		bluealsa) /usr/bin/bluealsa-aplay -vv 00:00:00:00:00:00 -d dmix:CARD=Loopback,DEV=0 ;;
+		bluealsa) /usr/bin/bluealsa-aplay -vv 00:00:00:00:00:00 -d "$SINK" ;;
 		*) /usr/bin/arecord -D "$SOURCE" -f "$FORMAT" -d "$DURATION" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" | /usr/bin/aplay -D "$SINK" -f "$FORMAT" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" ;;
 	esac
 	echo "Loop broken, restarting"
