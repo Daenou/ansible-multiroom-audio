b1eaf3e9d9f4221b1ddaa945b4e98303d13a70a4
Nicolas Iselin <nicolas.iselin@adfinis.com>
2021-11-13 15:42:22 +0100

    Refactored/beautified alsadetect

--- roles/acable/files/usr/local/bin/acable.sh
+++ roles/acable/files/usr/local/bin/acable.sh
@@ -16,36 +16,36 @@ then
 	source "$CONFFILE"
 fi
 
-find_bt_device () {
-for device in $( bluetoothctl devices | awk ' { print $2 } ' )
-do
-  if [[ $( bluetoothctl info $device | grep Connected | awk ' { print $2 } ' ) == "yes" ]]
-  then
-    echo $device
-  fi
+wait_for_and_get_first_connected_mac () {
+MAC="NULL"
+while [[ "$MAC" == "NULL" ]]; do
+        for tryMAC in  $( bluetoothctl devices | sort -k 3 | awk ' { print $2 } ' ) 
+        do
+                if [[ $( bluetoothctl info ${tryMAC} | grep Connected | awk ' { print $2 } ' ) == "yes" ]]; then
+                        MAC=$tryMAC
+                        break
+                fi
+        done
+        if [[ "$MAC" == "NULL" ]]; then
+                sleep 1
+        fi
 done
+echo "$MAC"
 }
 
-
 while true; do
-	case "$1" in
-		bluealsa) /usr/bin/bluealsa-aplay -vv 00:00:00:00:00:00 -d "$SINK" ;;
-                bluediscover )
-                        MAC=NULL
-                        while [[ $MAC == "NULL" ]]; do
-                                echo "Searching BT Device"
-                                for DEVICE in $( find_bt_device ); do
-                                        echo "Found connected $DEVICE"
-                                        MAC=$DEVICE
-                                done
-                                if [[ "$MAC" == "NULL" ]]; then
-                                        sleep 1
-                                fi
-                        done
-                        SOURCE="bluealsa:SRV=org.bluealsa,DEV=${MAC},PROFILE=a2dp"
-                        echo "Using discovered $SOURCE"
-                        /usr/bin/arecord -D "$SOURCE" -f "$FORMAT" -d "$DURATION" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" | /usr/bin/aplay -D "$SINK" -f "$FORMAT" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" ;;
-		*)      /usr/bin/arecord -D "$SOURCE" -f "$FORMAT" -d "$DURATION" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" | /usr/bin/aplay -D "$SINK" -f "$FORMAT" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" ;;
+        case "$1" in
+	        bluealsa) /usr/bin/bluealsa-aplay -vv 00:00:00:00:00:00 -d "$SINK" 
+                        ;;
+                *)      if [[ "$1" == bluediscover ]]; then
+                                echo "Waiting for bt device to connect"
+                                MAC=$( wait_for_and_get_first_connected_mac ) 
+                                SOURCE="bluealsa:SRV=org.bluealsa,DEV=${MAC},PROFILE=a2dp"
+                                echo "Using discovered SOURCE $SOURCE"
+                        fi
+                        /usr/bin/arecord -D "$SOURCE" -f "$FORMAT" -d "$DURATION" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" | \
+                          /usr/bin/aplay -D "$SINK"   -f "$FORMAT" -d "$DURATION" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" 
+                        ;;
 	esac
 	echo "Loop broken, restarting"
         sleep 1
