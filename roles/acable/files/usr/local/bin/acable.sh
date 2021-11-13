2c4db36b324e313d711165e4ef26eab5e33e65f0
Daenou <github@drth.ch>
2021-11-13 17:53:20 +0100

    #12 Renamed bluealsa and bluediscover

--- roles/acable/files/usr/local/bin/acable.sh
+++ roles/acable/files/usr/local/bin/acable.sh
@@ -33,18 +33,23 @@ done
 echo "$MAC"
 }
 
+arecaplaypipe() {
+	/usr/bin/arecord -D "$SOURCE" -f "$FORMAT" -d "$DURATION" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" | \
+	/usr/bin/aplay -D "$SINK"   -f "$FORMAT" -d "$DURATION" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" 
+}
+
 while true; do
         case "$1" in
-	        bluealsa) /usr/bin/bluealsa-aplay -vv 00:00:00:00:00:00 -d "$SINK" 
+	        bluealsa-cable) /usr/bin/bluealsa-aplay -vv 00:00:00:00:00:00 -d "$SINK" 
                         ;;
-                *)      if [[ "$1" == bluediscover ]]; then
-                                echo "Waiting for bt device to connect"
-                                MAC=$( wait_for_and_get_first_connected_mac ) 
-                                SOURCE="bluealsa:SRV=org.bluealsa,DEV=${MAC},PROFILE=a2dp"
-                                echo "Using discovered SOURCE $SOURCE"
-                        fi
-                        /usr/bin/arecord -D "$SOURCE" -f "$FORMAT" -d "$DURATION" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" | \
-                          /usr/bin/aplay -D "$SINK"   -f "$FORMAT" -d "$DURATION" -t "$FILETYPE" --buffer-size="$BUFFERSIZE" --period-size="$PERIODSIZE" 
+		bluealsa-workaround-cable) 
+                        echo "Waiting for bt device to connect"
+                        MAC=$( wait_for_and_get_first_connected_mac ) 
+                        SOURCE="bluealsa:SRV=org.bluealsa,DEV=${MAC},PROFILE=a2dp"
+                        echo "Using discovered SOURCE $SOURCE"
+			arecaplaypipe
+			;;
+		*)  arecaplaypipe
                         ;;
 	esac
 	echo "Loop broken, restarting"
